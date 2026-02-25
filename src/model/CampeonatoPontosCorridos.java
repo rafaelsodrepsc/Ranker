441c42e47aefc2d5086110f2355115bbae208b8b
Rafael <rafaelsodrepsc@gmail.com>
2026-02-25 18:42:12 -0300

    feat: implementa gerarConfrontos com algoritmo Round Robin (ida e volta)

--- src/model/CampeonatoPontosCorridos.java
+++ src/model/CampeonatoPontosCorridos.java
@@ -3,6 +3,8 @@ package model;
 import exception.TimesInsuficientesException;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CampeonatoPontosCorridos extends Campeonato {
     public CampeonatoPontosCorridos(String nome, int diasDeDescanso, LocalDate dataDeInicio) {
@@ -15,7 +17,37 @@ public class CampeonatoPontosCorridos extends Campeonato {
         }
 
         //Algoritmo Round Robin de torneios - comumente usado na criação de pontos corridos
+        int numRodadas = times.size()-1;
+        int metade = times.size()/2;
 
+        //Jogos de ida
+        List<Time> listaIda = new ArrayList<>(times);
+        for(int i = 1; i <= numRodadas; i++){
+            LocalDate dataRodada = dataDeInicio.plusDays((long) i * diasDeDescanso);
+            for (int j = 0; j < metade;j++){
+                Time timeA = listaIda.get(j);
+                Time timeB = listaIda.get(listaIda.size()- 1 - j);
+
+                Partida partida = new Partida(timeA,timeB,i,dataRodada);
+                confrontos.add(partida);
+            }
+            Time ultimo = listaIda.removeLast();
+            listaIda.add(1,ultimo);
+        }
+        //Jogo da volta
+        List<Time>listaVolta = new ArrayList<>(times);
+        for (int i = 1; i<= numRodadas; i++){
+            LocalDate dataRodada = dataDeInicio.plusDays((long) (numRodadas + i) * diasDeDescanso);
+            for(int j = 0; j < metade;j++){
+                Time timeA = listaVolta.get(j);
+                Time timeB = listaVolta.get(listaVolta.size() - 1 - j);
+
+                Partida partida = new Partida(timeB,timeA, numRodadas + i,dataRodada);
+                confrontos.add(partida);
+            }
+            Time ultimo = listaVolta.removeLast();
+            listaVolta.add(1,ultimo);
+        }
     }
 
     // ---------------------------------------------------------
