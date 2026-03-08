a23c1e8322ce6a354681051090c5479fa4f4b9e1
Rafael <rafaelsodrepsc@gmail.com>
2026-03-07 21:12:44 -0300

    fix: corrige o algoritmo Round Robin, permitindo numero impar de times

--- src/model/CampeonatoPontosCorridos.java
+++ src/model/CampeonatoPontosCorridos.java
@@ -18,32 +18,47 @@ public class CampeonatoPontosCorridos extends Campeonato {
         if ( this.times.size() < 4 ){ throw new TimesInsuficientesException("Minimo de 4 times para o campeonato."); }
 
         //Algoritmo Round Robin de torneios - comumente usado na criação de pontos corridos
-        int numRodadas = times.size()-1;
-        int metade = times.size()/2;
 
-        List<Time> listaIda = new ArrayList<>(this.times); //Jogos de ida
+        List<Time> listaIda = new ArrayList<>(this.times);
+
+        if (listaIda.size() % 2 != 0) {
+            listaIda.add(null); // time fantasma
+        }
+        int numRodadas = listaIda.size()-1;
+        int metade = listaIda.size()/2;
+
+        //Jogos de ida
         for( int i = 1; i <= numRodadas; i++ ){
             LocalDate dataRodada = this.dataDeInicio.plusDays((long) i * this.diasDeDescanso);
             for ( int j = 0; j < metade; j++ ){
                 Time timeA = listaIda.get(j);
                 Time timeB = listaIda.get(listaIda.size()- 1 - j);
 
-                Partida partida = new Partida(timeA,timeB,i ,dataRodada,timeA.getLocalTime());
-                this.confrontos.add(partida);
+                if (timeA != null && timeB != null) {
+                    Partida partida = new Partida(timeA, timeB, i, dataRodada, timeA.getLocalTime());
+                    this.confrontos.add(partida);
+                }
             }
             Time ultimo = listaIda.removeLast();
             listaIda.add(1, ultimo);
         }
-
         List<Time>listaVolta = new ArrayList<>(this.times); //Jogo da volta
+
+        if (listaVolta.size() % 2 != 0) {
+            listaVolta.add(null); // time fantasma
+        }
+
         for ( int i = 1; i <= numRodadas; i++ ){
             LocalDate dataRodada = this.dataDeInicio.plusDays((long) (numRodadas + i) * this.diasDeDescanso);
             for( int j = 0; j < metade; j++ ){
                 Time timeA = listaVolta.get(j);
                 Time timeB = listaVolta.get(listaVolta.size() - 1 - j);
 
+                if (timeA != null && timeB != null){
+
                 Partida partida = new Partida(timeB,timeA, numRodadas + i, dataRodada, timeB.getLocalTime());
                 this.confrontos.add(partida);
+                }
             }
             Time ultimo = listaVolta.removeLast();
             listaVolta.add(1, ultimo);
---
+++ src/model/Cores.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Cores {
+    public static final String RESET   = "\u001B[0m";
+    public static final String NEGRITO = "\u001B[1m";
+    public static final String VERDE   = "\u001B[32m";
+    public static final String VERMELHO= "\u001B[31m";
+    public static final String AMARELO = "\u001B[33m";
+    public static final String CINZA   = "\u001B[90m";
+}
